f595f67605facc1399240730ee2293508a151126
kerlcarl <carl.klein@students.fhnw.ch>
2025-11-03 17:35:48 +0100

    commit

--- Abgabe Definitiv.py
+++ Abgabe Definitiv.py
@@ -1,3 +1,19 @@
-print("Hello World")
-#Hallo
-#hallo2
+import random
+#Test
+DATEI = "karteikartenfile.txt"
+
+
+
+
+
+
+
+
+#ab hier Carl's Teil also Lernmodus und Prüfungsmodus:
+def prüfungsmodus(k, speichere_karten_func):
+    if not k:
+        print("Noch keine Karten vorhanden.")
+        return
+#Funktion Prüfungsmodus erstellt und variable k und speichere_karten_func erstellt. 
+#falls k nicht vorhanden ist (da bisher keine Karteikarte erstellt wurde print)
+
