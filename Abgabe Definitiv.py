a00eee7eb3aefb862f3da1301a099e6c1146f873
kerlcarl <carl.klein@students.fhnw.ch>
2025-11-07 20:17:53 +0100

    Änderungen vorgenommen

--- Abgabe Definitiv.py
+++ Abgabe Definitiv.py
@@ -1,25 +1,3 @@
-import random
-#Test
-DATEI = "karteikartenfile.txt"
-
-
-def löschen(k):
-    if not k: print("Noch keine Karten vorhanden."); return
-    
-
-
-
-
-
-
-#ab hier Carl's Teil also Lernmodus und Prüfungsmodus:
-def prüfungsmodus(k, speichere_karten_func):
-    if not k:
-        print("Noch keine Karten vorhanden.")
-        return
-#Funktion Prüfungsmodus erstellt und variable k und speichere_karten_func erstellt. 
-#falls k nicht vorhanden ist (da bisher keine Karteikarte erstellt wurde print)
-
 #Code Chatgpt einfache Version
 
 import random
